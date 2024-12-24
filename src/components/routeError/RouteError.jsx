540330cd4bed9b567d19a96526e4de38c21002ba
Vishwas Bhoir <codervishwas@gmail.com>
2024-12-24 11:43:39 +0530

    Update RouteError - Error page Style and msg changes
    Now msg is descriptive and user can take proper action reading error message.

--- src/components/routeError/RouteError.jsx
+++ src/components/routeError/RouteError.jsx
@@ -7,11 +7,13 @@ const ErrorPage = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
 			<h1 className="text-4xl font-bold text-red-500">Oops!</h1>
-			<p className="text-lg mt-4">Something went wrong.</p>
+			<p className="text-lg text-gray-500 mt-4">Something went wrong.</p>
+			<p className="text-lg text-gray-500 mt-4">
+				<span className="text-xl text-gray-700">Please, Refresh...</span> or try
+				after some time.
+			</p>
 			{error && (
-				<p className="text-sm text-gray-500 mt-2">
-					{error.statusText || error.message}
-				</p>
+				<p className="text-sm mt-2">{error.statusText || error.message}</p>
 			)}
 			<a
 				href="/"
